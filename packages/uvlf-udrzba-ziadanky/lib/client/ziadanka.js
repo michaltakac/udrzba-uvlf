03a8fc46fe1b0d98a95f17aebeb53285839a8d16
Michal Takac <takacmichal92@gmail.com>
2015-09-16 17:35:45 +0200

    updated print view, added vydajka table and notes rows

--- packages/uvlf-udrzba-ziadanky/lib/client/ziadanka.js
+++ packages/uvlf-udrzba-ziadanky/lib/client/ziadanka.js
@@ -3,6 +3,7 @@ Template.ziadanka.onCreated(function () {
   self.autorun(function() {
     var ziadankaId = FlowRouter.getParam('_id');
     self.subscribe('ziadanka', ziadankaId);
+    self.subscribe('vydajka', ziadankaId);
   });
 });
 
@@ -13,5 +14,18 @@ Template.ziadanka.onRendered(function() {
 Template.ziadanka.helpers({
   ziadanka: function() {
     return Ziadanky.findOne();
+  },
+  vydajka: function() {
+    return Vydajky.find().fetch().map(function(i, poradie) {
+      return {
+        _id: i._id,
+        polozkaId: i.polozkaId,
+        index: poradie + 1,
+        kategoria: i.kategoria,
+        nazovPolozky: i.nazovPolozky,
+        jednotka: i.jednotka,
+        pocet: i.pocet
+      }
+    });
   }
 });
