efb2e1bf6b84d5458134a6dbceec44272121fbdd
Michal Takac <takacmichal92@gmail.com>
2015-09-28 09:47:27 +0200

    updates

--- packages/uvlf-udrzba-core/lib/collections/schemas.js
+++ packages/uvlf-udrzba-core/lib/collections/schemas.js
@@ -1,20 +1,3 @@
-function filterPracovnikov(utvar) {
-  var pracovnici = Pracovnici.find({utvar: utvar}).fetch();
-  return pracovnici.map(function(ref) {
-    var titulPred = "";
-    var titulZa = "";
-    if (ref.titulPred) {
-      titulPred = ref.titulPred;
-    }
-    if (ref.titulZa) {
-      titulZa = ', '+ref.titulZa;
-    }
-    return {
-      label: titulPred + ' ' + ref.meno + ' ' + ref.priezvisko + titulZa,
-      value: ref._id
-    };
-  });
-}
 SimpleSchema.debug = true;
 
 Schemas = {};
